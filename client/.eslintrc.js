8dde28ec1b8a3fe622a7a11d714bde0581d1870d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-07-07 03:39:01 -0300

    feat: react setup

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+		process: true,
+		module: true,
+		export: true,
+		__dirname: true,
+		require: true,
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'no-unused-vars': 'off',
+	},
+}
