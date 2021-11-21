5870986e2aad425aa266c7ce40f608e202b6c013
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-11-21 02:12:22 -0300

    feat: add test should receive FetchPostsUseCase correctly

--- server/__test__/presentation/routers/FetchPostsRouter.spec.js
+++ server/__test__/presentation/routers/FetchPostsRouter.spec.js
@@ -1,11 +1,35 @@
 const FetchPostsRouter = require('../../../src/presentation/routers/FetchPostsRouter')
 
+class FetchPostsUseCase {
+	async fetchPosts() {}
+}
+
+const makeSut = () => {
+	const fetchPostsUseCase = new FetchPostsUseCase()
+	const sut = new FetchPostsRouter(fetchPostsUseCase)
+
+	return {
+		sut,
+		fetchPostsUseCase,
+	}
+}
+
 describe('FetchPostsRouter', () => {
 	it('should return 500 if no httpRequest is provided', async () => {
-		const sut = new FetchPostsRouter()
+		const { sut } = makeSut()
 
 		const httpResponse = await sut.route()
 
 		expect(httpResponse.statusCode).toBe(500)
+	})
+
+	it('should receive FetchPostsUseCase correctly', async () => {
+		const { sut } = makeSut()
+
+		const httpRequest = {}
+
+		await sut.route(httpRequest)
+
+		expect(sut.fetchPostsUseCase).toBeInstanceOf(FetchPostsUseCase)
 	})
 })
--- server/src/presentation/routers/FetchPostsRouter.js
+++ server/src/presentation/routers/FetchPostsRouter.js
@@ -1,6 +1,10 @@
 const HttpResponse = require('../http/HttpResponse')
 
 class FetchPostsRouter {
+	constructor(fetchPostsUseCase) {
+		this.fetchPostsUseCase = fetchPostsUseCase
+	}
+
 	async route(httpRequest) {
 		if (!httpRequest) {
 			return HttpResponse.serverError()
