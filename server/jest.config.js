cf31492297d2da378314dfd8a11dba289ffa71cd
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-11-21 00:53:10 -0300

    chore: add gitignore with updates & remove babel

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,11 +1,4 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
-	bail: true,
-	clearMocks: true,
-	coverageDirectory: 'test/coverage',
-	setupFiles: ['./test/env.js'],
+	coverageDirectory: 'coverage',
 	testEnvironment: 'node',
-	testMatch: ['**/test/**/*.test.js?(x)'],
 }
